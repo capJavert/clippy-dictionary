9f14556082cb91cb22d32842d8ff2863f7e077b2
Ante Barić <ante.baric@burza.hr>
2021-01-11 00:44:01 +0100

    fix(build): make sure build script points to root clippy.json
    format clippy.json

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 var rimraf = require('rimraf')
-const dictionary = require(path.join(__dirname, '../src/clippy.json'))
+const dictionary = require(path.join(__dirname, '../clippy.json'))
 const buildPath = path.join(__dirname, '../dist')
 
 rimraf.sync(buildPath)
